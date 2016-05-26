c198e0d8fbfa09d73da89731d139e1d6420a37cf
Tayeb Zaidi <tayebzaidi@gmail.com>
2016-05-26 11:51:40 +0200

    ciclos functioning, not optimized by grado yet

--- finalPractica/2_2_ciclos.py
+++ finalPractica/2_2_ciclos.py
@@ -1,75 +1,67 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 from collections import defaultdict
+import string
 import sys
 
-class Ciclos_Paso1(MRJob):
+class Ciclos(MRJob):
     def mapper(self, _, line):
         if line[0] != '#':
-            line_split = line.strip('()').split(',') #Strip parenthesis and split by the comma
+            line_stripped = line.translate(string.maketrans("",""), '"') #Eliminar los '"'
+            line_split = line_stripped.split(',') #split by the comma
             sorted_line = sorted(line_split)
             node1 = sorted_line[0]
             node2 = sorted_line[1]
             if node1 != node2:
-                yield node1, node2
-        
+                yield (node1, node2), None
+                yield '.pass_through.', (node1, node2)
         
     def reducer(self, key, values):
-        nodes = list(values)
-        for i in range(len(nodes)):
-            for j in range(i, len(nodes)):
-                if key != '"A"':
-                    print key, i, j   
-                if i != j:
-                    yield (nodes[i], nodes[j]), key
+        if key != '.pass_through.':
+            yield key[0], key[1]
+        else:
+            for val in values:
+                yield '.pass_through.', val
         
+    def reducer2(self, key, values):
+        if key != '.pass_through.':
+            nodes = list(values)
+            if len(nodes) > 1:
+                for i in range(len(nodes)):
+                    for j in range(i, len(nodes)):
+                        if i != j:
+                            yield (nodes[i], nodes[j]), key
+        else:
+            for node1, node2 in values:
+                yield (node1, node2), 'original'
             
-      
-class Ciclos_Paso2(MRJob):
-    def ciclos_count(self, key, values):
-            pass
-    
-    def grado_calc(self, key, values):
-        grados = defaultdict(int)
-        node_pairs = []
-        for node1, node2 in values:
-            print node1
-            grados[node1] += 1
-            grados[node2] += 1
-            node_pair = [node1, node2]
-            node_pairs.append(node_pair)
-        print grados
-        for node_pair in node_pairs:
-            yield node_pair, (grados[node_pair[0]], grados[node_pair[1]])
-            
-        for node in node_connections.keys():
-            if len(node_connections[node]) > 1:
-                for node_sub in node_connections[node]:
-                    if node in node_connections[node_sub]:
-                        pass
-            
-    def ciclos_count(self, key, values):
-        pass
-            
+    def reducer3(self, key, values):
+        vals = list(values)
+        if len(vals) > 1:
+            for val in vals:
+                node1 = key[0]
+                node2 = key[1]
+                if val != 'original':
+                    node3 = val
+                    ciclo = [node1,node2,node3]
+                    ciclo_sorted = sorted(ciclo)
+                    yield ciclo_sorted, None
+                    break
+        
     def steps(self):
         return [
             MRStep(mapper = self.mapper,
-                    reducer = self.reducer)#,
-            #MRStep(reducer = self.grado_calc)
+                    reducer = self.reducer),
+            MRStep(reducer = self.reducer2),
+            MRStep(reducer = self.reducer3)
         ]
+            
+                
+
             
         
 if __name__=='__main__':
-    print 'Starting ciclos1 job'
-    job_ciclos1 = Ciclos_Paso1(args=sys.argv[1:])
-    runner_ciclos1 = job_ciclos1.make_runner()
-    runner_ciclos1.run()
-    ciclos1_output = []
-    for line in runner_ciclos1.stream_output():
-        ciclos1_output = ciclos1_output + [job_ciclos1.parse_output_line(line)]
-    print 'Results ciclos1:', ciclos1_output
+    #print 'Starting ciclos1 job'
+    sys.stderr = open('localerrorlog.txt', 'w')
+    Ciclos.run()
     
-    f = open('results_ciclos1.txt','w')
-    for (node1, node2), parent in ciclos1_output:
-        f.write(node1+'\t'+node2+'\t'+parent+'\n')
-    f.close()
